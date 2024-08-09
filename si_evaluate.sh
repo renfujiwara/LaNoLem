0409a637cd62eaa0c46f56ce83beaa7db19a3057
ren <ren@local.com>
2024-08-09 10:13:03 +0900

    0809-3

--- si_evaluate.sh
+++ si_evaluate.sh
@@ -1,10 +1,5 @@
 num_works=-1
 
-# if [ $# != 1 ]; then
-#     echo arg error
-#     exit 1
-# fi
-
 today=$(date "+%Y%m%d")
 mkdir -p ./log/si/${today}
 # rtype='Lasso'
@@ -12,19 +7,13 @@ mkdir -p ./log/si/${today}
 # rtype='Ridge'
 rtype='normal'
 
+logfn="./log/si/${today}/SI_eval_${rtype}.txt"
+nohup poetry run python -W ignore _SI.py --num_works $num_works --date $today --rtype $rtype> $logfn &
+
+
 # rtype='MIOSR'
 # rtype='SSR'
 # rtype='STLSQ'
-
-# evaluate=$1
-# for data_name in 'lotka_volterra' 'hopf' 'cubic' 'vanderpol' 'rossler'
-# do
-# echo "------------------"
-# echo "data:" $data_name
-# echo "------------------"
-logfn="./log/si/${today}/SI_eval_${rtype}.txt"
 # logfn="./log/si/SI_eval_syn_${today}.txt"
-# for rtype in 'MIOSR' 'SSR' 'STLSQ'
 # nohup poetry run python -W ignore _SI_sindy.py --num_works $num_works --date $today --rtype $rtype> $logfn &
-nohup poetry run python -W ignore _SI.py --num_works $num_works --date $today --rtype $rtype> $logfn &
 # done
